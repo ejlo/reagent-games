6d2e361cd142641aa06b4d888dc954a051b38528
Erik Ouchterlony <erik.ouchterlony@bredband.net>
2015-02-05 23:22:00 +0100

    no need for multipage stuff

--- minesweeper/src/cljs/minesweeper/core.cljs
+++ minesweeper/src/cljs/minesweeper/core.cljs
@@ -1,56 +1,15 @@
 (ns minesweeper.core
   (:require [reagent.core :as reagent]
             [cljsjs.react]
-            [secretary.core :as secretary :include-macros true]
-            [goog.events :as events]
-            [goog.history.EventType :as EventType]
             [minesweeper.state :as state :refer [cur]]
-            [minesweeper.test :as test])
-  (:import goog.History))
-
-;; -------------------------
-;; Views
-
-(defmulti page identity)
-
-(defmethod page :page1 [_]
-  [:div [:h2 @(cur [:text]) "Page 1"]
-   [:div [:a {:href "#/page2"} "go to page 2 →"]]])
-
-(defmethod page :page2 [_]
-  [:div [:h2 @(cur [:text]) "Page 2"]
-   [:div [:a {:href "#/"} "← go to page 1"]]])
-
-(defmethod page :default [_]
-  [:div "Invalid/Unknown route"])
+            [minesweeper.test :as test]))
 
 (defn main-page []
-  [:div#main [page @(cur [:current-page])]
+  [:div#main
+   #_[board]
+   #_[info]
+   #_[new-game-button]
    [test/test-component]])
 
-;; -------------------------
-;; Routes
-(secretary/set-config! :prefix "#")
-
-(secretary/defroute "/" []
-  (reset! (cur [:current-page]) :page1))
-
-(secretary/defroute "/page2" []
-  (reset! (cur [:current-page]) :page2))
-
-;; -------------------------
-;; Initialize app
 (defn init! []
   (reagent/render-component [main-page] (.getElementById js/document "app")))
-
-;; -------------------------
-;; History
-(defn hook-browser-navigation! []
-  (doto (History.)
-    (events/listen
-     EventType/NAVIGATE
-     (fn [event]
-       (secretary/dispatch! (.-token event))))
-    (.setEnabled true)))
-;; need to run this after routes have been defined
-(hook-browser-navigation!)
--- minesweeper/test/minesweeper/core_test.cljs
+++ minesweeper/test/minesweeper/core_test.cljs
@@ -9,28 +9,11 @@
 (deftest trivial-pass
   (is (= 1 1)))
 
-(deftest trivial-fail
-  #_(is (= 2 1)))
-
-(deftest trivial-error
-  #_(is (1 = 1) "Don't do this!!!"))
-
-(deftest ^:async trivial-async-test
-  (let [now #(.getTime (js/Date.))
-        t (now)]
-    (js/setTimeout
-      (fn []
-        (is (>= (now) (+ t 10)))
-        (done))
-      10)))
-
-(defdomtest page1 {[:current-page] :page1}
-  (is (= (some-> [:#main :h2] sel1 dommy/text)
-         "Hello, this is: Page 1")))
-
-(defdomtest page2 {[:current-page] :page2}
-  (is (= (some-> [:#main :h2] sel1 dommy/text)
-         "Hello, this is: Page 2")))
-
 (defdomtest test-component {[:test] {:pass 2}}
   (is (= "2" (some-> [".test" ".pass" :span] sel second dommy/text))))
+
+(defdomtest app-exists {}
+  (is (sel1 :#app)))
+
+(defdomtest main-exists {}
+  (is (sel1 :#main)))
